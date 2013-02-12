e89f43024a0ee87f91c65de931a334433b2529ee
Denny <dennykoch@berlin.de>
2013-02-12 18:16:53 +0100

    Last commit before fallback to processing 1.5

--- Spielraum/src/xx/codeflower/spielraum/motion/scene/ScenenDoesNotExistsException.java
+++
@@ -1,7 +0,0 @@
-package xx.codeflower.spielraum.motion.scene;
-
-public class ScenenDoesNotExistsException extends Exception {
-
-	private static final long serialVersionUID = 1L;
-
-}
--- SpielraumP5/src/xx/codeflower/spielraum/motion/filter/MotionDataFilter.java
+++
@@ -1,9 +0,0 @@
-package xx.codeflower.spielraum.motion.filter;
-
-import xx.codeflower.spielraum.motion.data.MotionDataSet;
-
-public abstract class MotionDataFilter {
-
-	public abstract void filter(MotionDataSet mds);
-	
-}
