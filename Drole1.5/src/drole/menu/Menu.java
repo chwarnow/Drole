e76ffebaefd720a17b19c5ba1cd6d8e5c69b13e5
Denny <dennykoch@berlin.de>
2013-02-27 14:27:54 +0100

    Menu with rotation

--- Drole1.5/src/drole/menu/Menu.java
+++ Drole1.5/src/drole/menu/Menu.java
@@ -2,7 +2,6 @@ package drole.menu;
 
 import processing.core.PApplet;
 import codeanticode.glgraphics.GLModel;
-import codeanticode.glgraphics.GLworlds[i];
 
 import com.madsim.engine.Engine;
 import com.madsim.engine.drawable.Drawable;
@@ -21,50 +20,43 @@ public class Menu extends Drawable {
 		super(e);
 		
 		for(int i = 0; i < NUM_WORLDS; i++) {
-			worlds[i] = new GLModel(e.p, 4*NUM_WORLDS, GLModel.QUADS, GLModel.STATIC);
+			worlds[i] = new GLModel(e.p, 4, GLModel.QUADS, GLModel.STATIC);
 			
-			worlds[i].initTextures(NUM_WORLDS);
+			worlds[i].initTextures(1);
 			worlds[i].setTexture(0, e.requestTexture("data/images/MenuTestTarget.png"));
-			worlds[i].setTexture(1, e.requestTexture("data/images/MenuTestTarget.png"));
-			worlds[i].setTexture(2, e.requestTexture("data/images/MenuTestTarget.png"));
-			worlds[i].setTexture(3, e.requestTexture("data/images/MenuTestTarget.png"));
-			worlds[i].setTexture(4, e.requestTexture("data/images/MenuTestTarget.png"));
 			
 			worlds[i].beginUpdateVertices();
-				int world = 0;
-				for(int i = 0; i < 4*NUM_WORLDS; i+=4) {
-					worlds[i].beginUpdateTexCoords(world);
+
+					worlds[i].beginUpdateTexCoords(0);
 					
 						float l = ((r * PApplet.cos((PApplet.TWO_PI/NUM_WORLDS)+0.2f)) - (r * PApplet.cos((PApplet.TWO_PI/NUM_WORLDS)-0.2f)))/2f;
 						
-						float a = ((PApplet.TWO_PI/NUM_WORLDS)*world)-0.2f;
+						float a = ((PApplet.TWO_PI/NUM_WORLDS)*i)-0.2f;
 						float x = r * PApplet.cos(a);
 						float y = r * PApplet.sin(a);				
-						worlds[i].updateVertex(i, x, y, l);
-						worlds[i].updateTexCoord(i, 0, 1);
+						worlds[i].updateVertex(0, x, y, l);
+						worlds[i].updateTexCoord(0, 0, 1);
 						
-						a = ((PApplet.TWO_PI/NUM_WORLDS)*world)+0.2f;
+						a = ((PApplet.TWO_PI/NUM_WORLDS)*i)+0.2f;
 						x = r * PApplet.cos(a);
 						y = r * PApplet.sin(a);
-						worlds[i].updateVertex(i+1, x, y, l);
-						worlds[i].updateTexCoord(i+1, 1, 1);
+						worlds[i].updateVertex(1, x, y, l);
+						worlds[i].updateTexCoord(1, 1, 1);
 						
-						a = ((PApplet.TWO_PI/NUM_WORLDS)*world)+0.2f;
+						a = ((PApplet.TWO_PI/NUM_WORLDS)*i)+0.2f;
 						x = r * PApplet.cos(a);
 						y = r * PApplet.sin(a);
-						worlds[i].updateVertex(i+2, x, y, -l);
-						worlds[i].updateTexCoord(i+2, 1, 0);
+						worlds[i].updateVertex(2, x, y, -l);
+						worlds[i].updateTexCoord(2, 1, 0);
 						
-						a = ((PApplet.TWO_PI/NUM_WORLDS)*world)-0.2f;
+						a = ((PApplet.TWO_PI/NUM_WORLDS)*i)-0.2f;
 						x = r * PApplet.cos(a);
 						y = r * PApplet.sin(a);				
-						worlds[i].updateVertex(i+3, x, y, -l);
-						worlds[i].updateTexCoord(i+3, 0, 0);
-						
-						world++;
+						worlds[i].updateVertex(3, x, y, -l);
+						worlds[i].updateTexCoord(3, 0, 0);
 					
 					worlds[i].endUpdateTexCoords();
-				}
+
 			worlds[i].endUpdateVertices();
 			
 			worlds[i].initColors();
@@ -78,13 +70,19 @@ public class Menu extends Drawable {
 	public void draw() {
 		g.pushMatrix();
 		g.pushStyle();
-		
+			
+			g.translate(position.x, position.y, position.z);
 			g.rotateX(PApplet.radians(-90));
 		
-			for(int i = 0; i < NUM_WORLDS; i++) {
-				e.setupModel(worlds[i]);
-				worlds[i].render();
-			}
+			g.pushMatrix();
+			g.rotateY(rotation.y);
+			
+				for(int i = 0; i < NUM_WORLDS; i++) {
+					e.setupModel(worlds[i]);
+					worlds[i].render();
+				}
+			
+			g.popMatrix();
 			
 		g.popStyle();
 		g.popMatrix();
